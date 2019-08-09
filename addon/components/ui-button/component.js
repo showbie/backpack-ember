dfecee03f1c08164e2878aa6400021dc76de69e7
Jeff <rustyangel@gmail.com>
2019-08-09 16:15:47 -0600

    Add stories for other button styles

--- addon/components/ui-button/component.js
+++ addon/components/ui-button/component.js
@@ -5,6 +5,9 @@ import Component from '@glimmer/component';
 import cx from 'classnames';
 
 export default class UiButton extends Component {
+  /**
+   * Called `type` in showbie-ui.
+   */
   get appearance() {
     return this.args.appearance || 'default';
   }
@@ -13,16 +16,38 @@ export default class UiButton extends Component {
     return this.args.size || 'lg';
   }
 
+  /**
+   * Called `destructive` in showbie-ui.
+   */
   get isDestructive() {
     return this.args.isDestructive || false;
   }
 
+  /**
+   * Called `disableWhen` in showbie-ui.
+   */
   get isDisabled() {
     return this.args.isDisabled || false;
   }
 
   get isRunning() {
     return this.args.isRunning || false;
+  }
+
+  get block() {
+    return this.args.block || true;
+  }
+
+  get fixedSize() {
+    return this.args.fixedSize || false;
+  }
+
+  get inline() {
+    return this.args.inline || false;
+  }
+
+  get outlined() {
+    return this.args.outlined || false;
   }
 
   @or('isDisabled', 'isRunning')
@@ -35,7 +60,7 @@ export default class UiButton extends Component {
 
     return cx({
       'ui-button': true,
-      [`ui-button--${appearance}`]: appearance !== 'default',
+      [`ui-button--${appearance}`]: true, // appearance !== 'default',
       [`ui-button--${size}`]: true,
       // 'is-destructive': isDestructive,
       // 'is-hovered': isHovered,
--- stories/ui-button.story.js
+++ stories/ui-button.story.js
@@ -1,11 +1,29 @@
 import hbs from 'htmlbars-inline-precompile';
 import { storiesOf } from '@storybook/ember';
 
-storiesOf('Button', module).add('default', () => {
-  return {
-    template: hbs`
-        <UiButton aria-label="helpful text">Default</UiButton>
-        <UiButton @isDisabled={{true}}>Disabled</UiButton>
-      `,
-  };
+const appearanceOptions = [
+  'default',
+  'primary',
+  'faded',
+  'destroy', // Change this to its own argument.
+  'blank',
+  'link',
+  'white',
+  'inherit',
+];
+
+const stories = storiesOf('Button', module);
+
+appearanceOptions.forEach((style) => {
+  stories.add(style, () => {
+    return {
+      template: hbs`
+      <UiButton @appearance={{style}}>{{style}}</UiButton>
+      <UiButton @appearance={{style}} @isDisabled={{true}}>Disabled</UiButton>
+    `,
+      context: {
+        style: style,
+      },
+    };
+  });
 });
