71ba3f2061c6b3f0e5a6f1a74b15235f918864ce
Jeff <rustyangel@gmail.com>
2021-02-28 21:31:16 -0700

    Update: Add some utility classes to `Badge`

---
+++ addon/components/badge/index.ts
@@ -0,0 +1,61 @@
+import Component from '@glimmer/component';
+
+const badgeToneClasses = {
+  default: 'bg-grey-200',
+  info: '',
+  caution: '',
+  critical: '',
+};
+
+interface BadgeArgs {
+  value?: number;
+  max?: number;
+  displayZero?: boolean;
+  tone?: keyof typeof badgeToneClasses;
+}
+
+export default class Badge extends Component<BadgeArgs> {
+  value = this.args.value || 0;
+  max = this.args.max || 99;
+  tone = this.args.tone || 'default';
+
+  hasFormattedValue = !!this.formattedValue;
+
+  get classNames(): string | undefined {
+    const classNames = [
+      'inline-flex max-w-24 px-2 rounded-full',
+      badgeToneClasses[this.tone],
+    ].join(' ');
+
+    return classNames;
+  }
+
+  get textClassNames(): string {
+    return [
+      'text-xs font-800 leading-6 uppercase truncate',
+      this.tone === 'default' ? 'text-slate-900' : 'text-white',
+    ].join(' ');
+  }
+
+  get formattedValue(): string | undefined {
+    const value = this.value * 1;
+    const max = this.max * 1;
+    let string;
+
+    if (isNaN(value) || isNaN(max)) return undefined;
+
+    if (value === Infinity) {
+      string = '∞';
+    } else if (max < value) {
+      string = `${max}+`;
+    } else if ((value === 0 && this.args.displayZero) || value !== 0) {
+      string = `${value}`;
+    }
+
+    return string;
+  }
+
+  constructor(owner: unknown, args: BadgeArgs) {
+    super(owner, args);
+  }
+}
