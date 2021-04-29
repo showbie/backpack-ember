53ac6a4520b9c3bb4c69b400c4ddc4d55c6b8fc2
Jeff <rustyangel@gmail.com>
2021-04-29 12:56:20 -0600

    Add changeset

--- addon/components/announcement/index.ts
+++ addon/components/announcement/index.ts
@@ -1,5 +1,11 @@
 import Component from '@glimmer/component';
 
+/**
+ * An a11y helper that allows a component to add an ARIA live region
+ * while it is rendered.
+ *
+ * @see https://www.w3.org/TR/wai-aria/states_and_properties#attrs_liveregions
+ */
 export default class Text extends Component {
   appParent;
 
